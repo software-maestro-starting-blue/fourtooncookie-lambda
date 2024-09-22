624fcba25f70798012260afd9e75b2cb56bdc32d
JuneBridge <junebridge03@g.skku.edu>
2024-09-22 18:12:11 +0900

    feat: scenegenerator config에 등록

--- fourtooncookie-diaryimage-ai-apply-lambda/config.py
+++ fourtooncookie-diaryimage-ai-apply-lambda/config.py
@@ -2,6 +2,8 @@ import os
 from openai import OpenAI
 from llm.llm_service import LLMService
 from llm.gpt4o_llm_service import GPT4oLLMService
+from scenegenerator.scene_generator import SceneGenerator
+from scenegenerator.diary_scene_generator import DiarySceneGenerator
 from visionrequest.vision_request_service import VisionRequestService
 from visionrequest.dall_e_3_vision_request_service import DallE3VisionRequestService
 
@@ -11,4 +13,6 @@ llm_service: LLMService = GPT4oLLMService(openai)
 vision_request_services: list[VisionRequestService] = [
     DallE3VisionRequestService(openai, os.environ['S3_BUCKET_NAME'])
 ]
+
+scene_generator: SceneGenerator = DiarySceneGenerator(llm_service)
 
