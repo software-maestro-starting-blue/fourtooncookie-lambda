928e31e766d37a96089766962dcc5a86e58e70bf
JuneBridge <junebridge03@g.skku.edu>
2024-09-22 18:12:51 +0900

    fix: config에서 vision_request_service를 dict로 정의
    key 값은 visionType이고, value는 VisionRequestService 구현체이도록

--- fourtooncookie-diaryimage-ai-apply-lambda/config.py
+++ fourtooncookie-diaryimage-ai-apply-lambda/config.py
@@ -10,9 +10,11 @@ from visionrequest.dall_e_3_vision_request_service import DallE3VisionRequestSer
 openai: OpenAI = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
 llm_service: LLMService = GPT4oLLMService(openai)
-vision_request_services: list[VisionRequestService] = [
-    DallE3VisionRequestService(openai, os.environ['S3_BUCKET_NAME'])
-]
 
 scene_generator: SceneGenerator = DiarySceneGenerator(llm_service)
+
+
+vision_request_services: dict[str, VisionRequestService] = {
+    "DALL_E_3": DallE3VisionRequestService(openai, os.environ['S3_BUCKET_NAME'])
+}
 
