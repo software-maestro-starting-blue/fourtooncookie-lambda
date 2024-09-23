bd6dce0b893e6e967d25b9cf8b326ce167566799
JuneBridge <junebridge03@g.skku.edu>
2024-09-23 13:01:57 +0900

    feat: lambda function에 에러 핸들링 로직 추가

--- fourtooncookie-diaryimage-ai-apply-lambda/lambda_function.py
+++ fourtooncookie-diaryimage-ai-apply-lambda/lambda_function.py
@@ -2,22 +2,26 @@ from config import *
 
 
 def lambda_handler(body, context):
-    # body 정리하기
-    diary_id = body['diaryId']
+    try:
+        # body 정리하기
+        diary_id = body['diaryId']
 
-    character = body['character']
-    character_id = character['id']
-    character_name = character['name']
-    character_vision_type = character['visionType']
-    character_base_prompt = character['basePrompt']
+        character = body['character']
+        character_id = character['id']
+        character_name = character['name']
+        character_vision_type = character['visionType']
+        character_base_prompt = character['basePrompt']
 
-    content = body['content']
+        content = body['content']
 
-    # LLM과 prompt 활용하여 내용 정체
-    prompts = scene_generator.generate_scenes(content)
+        # LLM과 prompt 활용하여 내용 정체
+        prompts = scene_generator.generate_scenes(content)
 
-    # vision request로 적절한 요청 보내기
-    vision_request_service = vision_request_services[character_vision_type]
-    vision_request_service.request_vision(diary_id, character_id, character_base_prompt, prompts)
-    
-    return True
+        # vision request로 적절한 요청 보내기
+        vision_request_service = vision_request_services[character_vision_type]
+        vision_request_service.request_vision(diary_id, character_id, character_base_prompt, prompts)
+        
+        return True
+    except Exception as e:
+        print(e)
+        return False
