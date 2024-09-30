d6e1ddcd889ef6d39567e1a45f82673bb166b58d
JuneBridge <junebridge03@g.skku.edu>
2024-09-30 14:42:25 +0900

    feat: stable diffusion vision request service에서 prompt를 정제하는 로직 추가

--- fourtooncookie-diaryimage-ai-apply-lambda/visionrequest/stable_diffusion_vision_request_service.py
+++ fourtooncookie-diaryimage-ai-apply-lambda/visionrequest/stable_diffusion_vision_request_service.py
@@ -1,4 +1,6 @@
 from visionrequest.vision_request_service import VisionRequestService
+from llm.llm_service import LLMService
+from prompt import get_stable_diffusion_modify_prompt
 
 import boto3
 import json
@@ -7,19 +9,28 @@ class StableDiffusionVisionRequestService(VisionRequestService):
 
     __sqs: boto3.client
     __queue_url: str
+    __llm_service: LLMService
 
-    def __init__(self, sqsclient: boto3.client, queue_url: str):
+    def __init__(self, sqsclient: boto3.client, queue_url: str, llm_service: LLMService):
         self.__sqs = sqsclient
         self.__queue_url = queue_url
+        self.__llm_service = llm_service
+    
+    def __modify_prompt(self, prompt: str) -> str:
+        result = self.__llm_service.get_llm(get_stable_diffusion_modify_prompt(), prompt)
+        result_json: dict = json.loads(result)
+        return ", ".join(result_json.values())
 
     def request_vision(self, diary_id: int, character_id: int, character_base_prompt: str, prompts: list[str]):
         for i, prompt in enumerate(prompts):
+            modified_prompt = self.__modify_prompt(prompt)
+
             self.__sqs.send_message(
                 QueueUrl=self.__queue_url,
                 MessageBody=json.dumps({
                     'diaryId': diary_id,
                     'characterId': character_id,
-                    'prompt': character_base_prompt + ' ' + prompt,
+                    'prompt': character_base_prompt + ', ' + modified_prompt,
                     'gridPosition': i
                 })
             )
