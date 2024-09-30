6d979e589bedc44cef2ce47d0a4d25ed1f3340e3
JuneBridge <junebridge03@g.skku.edu>
2024-09-30 15:28:12 +0900

    feat: openai 객체에 Portkey를 적용

--- fourtooncookie-diaryimage-ai-apply-lambda/config.py
+++ fourtooncookie-diaryimage-ai-apply-lambda/config.py
@@ -1,5 +1,6 @@
 import os
 from openai import OpenAI
+from portkey_ai import PORTKEY_GATEWAY_URL, createHeaders
 import boto3
 from llm.llm_service import LLMService
 from llm.gpt4o_llm_service import GPT4oLLMService
@@ -9,9 +10,19 @@ from visionrequest.vision_request_service import VisionRequestService
 from visionrequest.dall_e_3_vision_request_service import DallE3VisionRequestService
 
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+PORTKEY_API_KEY = os.environ['PORTKEY_API_KEY']
 S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 
-openai: OpenAI = OpenAI(api_key=OPENAI_API_KEY)
+openai: OpenAI = OpenAI(
+    api_key=OPENAI_API_KEY,
+    base_url=PORTKEY_GATEWAY_URL,
+    default_headers=createHeaders(
+        provider="openai",
+        api_key=PORTKEY_API_KEY
+    )
+)
+
+
 s3client: boto3.client = boto3.client('s3')
 
 llm_service: LLMService = GPT4oLLMService(openai)
