f5d4dbfd563e6db1d3f63c1409977f59f8a9d5f2
JuneBridge <junebridge03@g.skku.edu>
2024-10-02 15:06:20 +0900

    [feat] LLM 로직에 Portkey 적용 (#4)
    * feat: openai 객체에 Portkey를 적용
    
    * feat: PortkeyLLMService 구현체 구현

--- fourtooncookie-diaryimage-ai-apply-lambda/config.py
+++ fourtooncookie-diaryimage-ai-apply-lambda/config.py
@@ -1,5 +1,6 @@
 import os
 from openai import OpenAI
+from portkey_ai import PORTKEY_GATEWAY_URL, createHeaders, Portkey
 import boto3
 from llm.llm_service import LLMService
 from llm.gpt4o_llm_service import GPT4oLLMService
@@ -10,10 +11,24 @@ from visionrequest.dall_e_3_vision_request_service import DallE3VisionRequestSer
 from visionrequest.sqs_stable_diffusion_vision_request_service import SQSStableDiffusionVisionRequestService
 
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+PORTKEY_API_KEY = os.environ['PORTKEY_API_KEY']
 S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 SQS_QUEUE_URL = os.environ['SQS_QUEUE_URL']
 
-openai: OpenAI = OpenAI(api_key=OPENAI_API_KEY)
+openai: OpenAI = OpenAI(
+    api_key=OPENAI_API_KEY,
+    base_url=PORTKEY_GATEWAY_URL,
+    default_headers=createHeaders(
+        provider="openai",
+        api_key=PORTKEY_API_KEY
+    )
+)
+
+portkey = Portkey(
+    api_key=PORTKEY_API_KEY
+)
+
+
 s3client: boto3.client = boto3.client('s3')
 sqsclient: boto3.client = boto3.client('sqs', region_name='ap-northeast-2')
 
---
+++ fourtooncookie-diaryimage-ai-apply-lambda/llm/portkey_llm_service.py
@@ -0,0 +1,32 @@
+from llm.llm_service import LLMService
+from portkey_ai import Portkey
+
+class PortkeyLLMService(LLMService):
+
+    __portkey: Portkey
+    __gpt_model: str
+
+    def __init__(self, portkey: Portkey, gpt_model: str = 'gpt-4o'):
+        self.__portkey = portkey
+        self.__gpt_model = gpt_model
+
+    def get_llm(self, system_prompt: str, user_prompt: str) -> str:
+        response = self.__portkey.chat.completions.create(
+            model=self.__gpt_model,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            timeout=20
+        )
+
+        return response.choices[0].message.content
+    
+    def get_llm_using_portkey(self, prompt_id: str, variables: dict) -> str:
+        response = self.__portkey.chat.completions.create(
+            prompt_id=prompt_id,
+            variables=variables,
+            timeout=20
+        )
+
+        return response.choices[0].message.content
