d25c74e60cc7d553cb1321e74402192eb982d418
JuneBridge <junebridge03@g.skku.edu>
2024-10-03 15:17:10 +0900

    fix: ImageResponseSQSService에서 모든 gridPosition에 대한 성공, 실패 여부를 보내도록 로직 수정

--- fourtooncookie-diaryimage-ai-apply-lambda/sqs/image_response_sqs_service.py
+++ fourtooncookie-diaryimage-ai-apply-lambda/sqs/image_response_sqs_service.py
@@ -11,19 +11,23 @@ class ImageResponseSQSService():
         self.__queue_url = queue_url
     
     def send_image_success_response(self, diary_id: int):
-        self.__sqs.send_message(
-            QueueUrl=self.__queue_url,
-            MessageBody=json.dumps({
-                "diaryId": diary_id,
-                "status": True
-            })
-        )
+        for grid_position in range(4):
+            self.__sqs.send_message(
+                QueueUrl=self.__queue_url,
+                MessageBody=json.dumps({
+                    "diaryId": diary_id,
+                    "gridPosition": grid_position,
+                    "status": True
+                })
+            )
     
     def send_image_failure_response(self, diary_id: int):
-        self.__sqs.send_message(
-            QueueUrl=self.__queue_url,
-            MessageBody=json.dumps({
-                "diaryId": diary_id,
-                "status": False
-            })
-        )
+        for grid_position in range(4):
+            self.__sqs.send_message(
+                QueueUrl=self.__queue_url,
+                MessageBody=json.dumps({
+                    "diaryId": diary_id,
+                    "gridPosition": grid_position,
+                    "status": False
+                })
+            )
