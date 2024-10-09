a3d88af500c5567926b1318854572a3415c62b3c
JuneBridge <junebridge03@g.skku.edu>
2024-10-09 18:40:13 +0900

    feat: JsonConvertExecuter 구현

---
+++ fourtooncookie-diaryimage-ai-apply-lambda/executer/json_convert_executer.py
@@ -0,0 +1,10 @@
+from executer.convert_executer import ConvertExecuter
+import json
+
+class JsonConvertExecuter(ConvertExecuter):
+
+    def execute(self, variables: str):
+        return json.loads(variables)
+
+    def validate_variables(self, variables) -> bool:
+        return isinstance(variables, str)
