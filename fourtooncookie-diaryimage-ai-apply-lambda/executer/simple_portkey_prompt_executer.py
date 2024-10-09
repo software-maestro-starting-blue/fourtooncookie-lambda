d4a88ecb816739144d81d5efcb3068b0f1333c7f
JuneBridge <junebridge03@g.skku.edu>
2024-10-09 18:10:59 +0900

    feat: Portkey를 활용하는 executer 구현체 구현

---
+++ fourtooncookie-diaryimage-ai-apply-lambda/executer/simple_portkey_prompt_executer.py
@@ -0,0 +1,9 @@
+from executer.portkey_prompt_executer import PortkeyPromptExecuter
+
+class SimplePortkeyPromptExecuter(PortkeyPromptExecuter):
+    
+    def execute(self, variables: str) -> str:
+        return super().execute({"prompt": variables})
+    
+    def validate_variables(self, variables) -> bool:
+        return isinstance(variables, str)
