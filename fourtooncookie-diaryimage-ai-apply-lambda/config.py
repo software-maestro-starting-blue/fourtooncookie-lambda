1d22ff5b8cdf7a162190cf8cb006c8cea1ee0a50
JuneBridge <junebridge03@g.skku.edu>
2024-10-10 11:27:09 +0900

    refactor: config.py import 정리

--- fourtooncookie-diaryimage-ai-apply-lambda/config.py
+++ fourtooncookie-diaryimage-ai-apply-lambda/config.py
@@ -1,14 +1,36 @@
 import os
 
+from openai import OpenAI
+from portkey_ai import PORTKEY_GATEWAY_URL, createHeaders, Portkey
+
+import boto3
+
+from sqs.image_response_sqs_service import ImageResponseSQSService
+
+from llm.llm_service import LLMService
+from llm.portkey_llm_service import PortkeyLLMService
+
+from executer.executer import Executer
+
+from executer.convert.json_convert_executer import JsonConvertExecuter
+from executer.convert.synopsis_to_scene_convert_executer import SynopsisToSceneConvertExecuter
+from executer.batch_executer import BatchExecuter
+from executer.prompt.simple_portkey_prompt_executer import SimplePortkeyPromptExecuter
+
+from visionrequest.dall_e_3.executer.scenes_as_image_prompt_convert_executer import ScenesAsImagePromptConvertExecuter
+from visionrequest.stable_diffusion.executer.scene_as_words_convert_executer import SceneAsWordsConvertExecuter
+
+from visionrequest.vision_request_service import VisionRequestService
+from visionrequest.dall_e_3.dall_e_3_vision_request_service import DallE3VisionRequestService
+from visionrequest.stable_diffusion.sqs_stable_diffusion_vision_request_service import SQSStableDiffusionVisionRequestService
+
+
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 PORTKEY_API_KEY = os.environ['PORTKEY_API_KEY']
 S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 IMAGE_RESPONSE_SQS_QUEUE_URL = os.environ['IMAGE_RESPONSE_SQS_QUEUE_URL']
 STABLE_DIFFUSION_SQS_QUEUE_URL = os.environ['STABLE_DIFFUSION_SQS_QUEUE_URL']
 
-
-from openai import OpenAI
-from portkey_ai import PORTKEY_GATEWAY_URL, createHeaders, Portkey
 
 openai: OpenAI = OpenAI(
     api_key=OPENAI_API_KEY,
@@ -22,46 +44,21 @@ portkey = Portkey(
     api_key=PORTKEY_API_KEY
 )
 
-
-import boto3
-
 s3client: boto3.client = boto3.client('s3')
 sqsclient: boto3.client = boto3.client('sqs', region_name='ap-northeast-2')
 
-from sqs.image_response_sqs_service import ImageResponseSQSService
 image_response_sqs_service: ImageResponseSQSService = ImageResponseSQSService(sqsclient, IMAGE_RESPONSE_SQS_QUEUE_URL)
-
-from llm.llm_service import LLMService
-from llm.portkey_llm_service import PortkeyLLMService
 
 llm_service: LLMService = PortkeyLLMService(portkey)
 
-
-from executer.executer import Executer
-
-from executer.convert.json_convert_executer import JsonConvertExecuter
-from executer.convert.synopsis_to_scene_convert_executer import SynopsisToSceneConvertExecuter
-from executer.batch_executer import BatchExecuter
-from executer.prompt.simple_portkey_prompt_executer import SimplePortkeyPromptExecuter
-
 SYNOPSIS_PROMPT_ID = os.environ["SYNOPSIS_PROMPT_ID"]
 REFINE_SCENE_PROMPT_ID = os.environ["REFINE_SCENE_PROMPT_ID"]
-
 scene_generate_executers: list[Executer] = [
     SimplePortkeyPromptExecuter(llm_service, SYNOPSIS_PROMPT_ID),
     JsonConvertExecuter(),
     BatchExecuter(SynopsisToSceneConvertExecuter()),
     BatchExecuter(SimplePortkeyPromptExecuter(llm_service, REFINE_SCENE_PROMPT_ID)),
 ]
-
-
-from visionrequest.dall_e_3.executer.scenes_as_image_prompt_convert_executer import ScenesAsImagePromptConvertExecuter
-from visionrequest.stable_diffusion.executer.scene_as_words_convert_executer import SceneAsWordsConvertExecuter
-from visionrequest.stable_diffusion.executer.scene_as_words_convert_executer import SceneAsWordsConvertExecuter
-
-from visionrequest.vision_request_service import VisionRequestService
-from visionrequest.dall_e_3.dall_e_3_vision_request_service import DallE3VisionRequestService
-from visionrequest.stable_diffusion.sqs_stable_diffusion_vision_request_service import SQSStableDiffusionVisionRequestService
 
 vision_request_services: dict[str, VisionRequestService] = {
     "DALL_E_3": DallE3VisionRequestService(
