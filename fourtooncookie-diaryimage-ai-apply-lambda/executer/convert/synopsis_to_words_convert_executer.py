d29691c2286d07fbcf116b1d0b89fdd706f1df8a
JuneBridge <junebridge03@g.skku.edu>
2024-10-13 18:25:00 +0900

    fix: SynopsisToWordsConvertExecuter의 join 문제 해결

--- fourtooncookie-diaryimage-ai-apply-lambda/executer/convert/synopsis_to_words_convert_executer.py
+++ fourtooncookie-diaryimage-ai-apply-lambda/executer/convert/synopsis_to_words_convert_executer.py
@@ -4,12 +4,16 @@ from executer.convert.convert_executer import ConvertExecuter
 class SynopsisToWordsConvertExecuter(ConvertExecuter):
 
     def execute(self, variables) -> list[str]:
-        situations = "".join([
-            person_data["action"] + ", " + person_data["facial expression"]
-            for person_data in variables['persons']
-        ])
+        words = []
+        for person_data in variables['persons']:
+            words.append(person_data["action"])
+            words.append(person_data["facial expression"])
+        
+        words.append(variables['place'])
+        words.append(variables['time'])
+        words.append(variables['weather'])
 
-        return ", ".join([variables['place'], variables['time'], variables['weather']] + situations)
+        return ", ".join(words)
     
     def validate_variables(self, variables) -> bool:
         if not isinstance(variables, dict): # 변수가 리스트인지 확인
