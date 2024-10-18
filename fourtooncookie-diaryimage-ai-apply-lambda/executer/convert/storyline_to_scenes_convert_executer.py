3930e32549c2e2066c88ab4acb3aa81ec74e48bf
JuneBridge <junebridge03@g.skku.edu>
2024-10-18 10:06:05 +0900

    fix: StorylineToScenesConvertExecuter 영어 결과를 번역할 수 있도록 로직을 수정

--- fourtooncookie-diaryimage-ai-apply-lambda/executer/convert/storyline_to_scenes_convert_executer.py
+++ fourtooncookie-diaryimage-ai-apply-lambda/executer/convert/storyline_to_scenes_convert_executer.py
@@ -5,7 +5,7 @@ from executer.convert.convert_executer import ConvertExecuter
 class StorylineToScenesConvertExecuter(ConvertExecuter):
 
     def execute(self, variables: str) -> list[str]:
-        pattern = r"기:\s*(.*?)\s*승:\s*(.*?)\s*전:\s*(.*?)\s*결:\s*(.*)"
+        pattern = r"Introduction:\s*(.*?)\n*Development:\s*(.*?)\n*Turn:\s*(.*?)\n*Conclusion:\s*(.*)"
 
         # 패턴에 맞는 부분 추출
         match = re.search(pattern, variables, re.DOTALL)
